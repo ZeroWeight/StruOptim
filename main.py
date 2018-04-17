2c0d3fd54fc9caf55cc9052d47bd4cadcd45dd96
zeroweight <zwt15@mails.tsinghua.edu.cn>
2018-04-17 15:26:59 +0800

    LeNet demo finished

--- main.py
+++ main.py
@@ -73,4 +73,4 @@ def create_network(para, verbose=False):
 
 if __name__ == '__main__':
     optimizer = StruOptim(create_network,valid_reader,mapping,100,100,network_input,[2,2,2,2],[130,130,1026,1026])
-    optimizer.start_optim([6,16,84,128],sys.argv[1],forward_step=[2,2,2,4],backward_step=2)
+    optimizer.start_optim([6,16,84,128],sys.argv[1],forward_step=[2,2,8,16],backward_step=4)
